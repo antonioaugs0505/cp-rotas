00d0ef2f6579f55ef0f4ea119939874b40883b41
luanrdias23 <luan.rdias@gmail.com>
2023-10-17 21:46:36 -0300

    alterando class

--- src/components/Rodape/page.jsx
+++ src/components/Rodape/page.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Cabecalho() {
   return (
-    <div className="cabecalho">
+    <div className="rodape">
       <header>
         <h2>Todos os direitos reservados - 2023</h2>
       </header>
